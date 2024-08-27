7d5b3f587c0bf09f2113eb2bc082b53b4f76489e
Valeria-15 <vandrade101122@espirituschools.org>
2024-08-27 21:50:15 +0000

    done

--- roller_coaster.py
+++ roller_coaster.py
@@ -8,11 +8,31 @@ height = int(input("What is your height in inches? "))
 # Step 2: Ask for their age.
 age = int(input("What is your age? "))
 
+
 #Step 3: Create an if statement that matches for their responses. You may need multiple if/elif statements. 
     # Use the logic flow diagram to help you
-
-
-
+if height >60:
+    print("You can ride the rollercoaster.")
+    if age<12:
+        print("please pay $5.")
+        bill = 5
+    elif age <<18:
+        print("please pay $7 .")
+        bill = 7
+    else:
+        print("please pay $12 .")
+        bill = 12
+    photo=input("Do you want a photo? Yes o No :")
+    if photo=="yes":
+        print("photos are $3 extra. ")
+        bill = bill+3
+        bill=str(bill)
+        print("Your final bill is $"+bill+".")
+    else:
+        bill=str(bill)
+        print("Your final bill is $"+bill+".")
+else:
+    print("You cannot ride.")
 # Step 4: Ask the user if they want a photo.
 
 # Step 5: Tell them what their final bill is.
